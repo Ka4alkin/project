944416096ec4fc9ce00b7644d3f325d1453f592e
Nazar <doskoch25@gmail.com>
2020-04-03 18:55:38 +0300

    first commit

---
+++ gggg/script.js
@@ -0,0 +1,45 @@
+'use strict';
+// var leftBorderWidth = 1;
+
+// let second = 2;
+
+// const pi = 3.14;
+
+// let person = {
+//     name: "Nazar",
+//     age: 20,
+//     isMarried: false
+// };
+
+// console.log(person.age);
+
+
+// alert ("Hello World");
+
+// let answer = confirm("Are you here?","Да");
+
+// console.log(answer);
+
+// let answer = prompt("Есть ли вам 18 лет?","Да");
+
+// console.log(typeof(answer));
+
+// let arr = ['stroka',2,'22'];
+
+// console.log(typeof(arr));
+
+// let raz = true,
+//     dva = true,
+//     tri = false;
+
+
+// console.log(raz && dva && tri);
+
+// let raz = false,
+//     dva = true,
+//     tri = true;
+
+
+// console.log(raz || dva && tri);
+
+alert(8);
